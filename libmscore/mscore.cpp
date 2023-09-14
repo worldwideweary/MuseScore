cbd5cf6524d4b0e8a3ffda79f35fe35f8f7c3c9b
Jesse Greene <worldwideweary@aol.com>
2023-09-14 02:13:32 -0700

    Layout - Ties: Adv. Preference to omit ledger lines in horizontal layout. + Aside: Notice that chords+offset will have inappropriate tie-positions from a grace note tying into it. Happens in 3.6.2 + 4.1 right now (2023.09.15)

--- libmscore/mscore.cpp
+++ libmscore/mscore.cpp
@@ -128,6 +128,8 @@ qreal   MScore::systemBracketMultiplier;
 qreal   MScore::slurShoulderExtraMax;
 qreal   MScore::slurShoulderExtraMin;
 
+bool    MScore::tiesAdjustForLedgerLines;
+
 bool    MScore::harmonyPlayDisableCompatibility;
 bool    MScore::harmonyPlayDisableNew;
 bool    MScore::playRepeats;
