59e79a4e13662f0212d17fbe1389df23ea19b06f
Jesse Greene <worldwideweary@aol.com>
2023-09-20 23:16:03 -0700

    [Adv. Preference] Ability to disable vertical note dragging with mouse

--- libmscore/mscore.cpp
+++ libmscore/mscore.cpp
@@ -74,6 +74,7 @@ QColor  MScore::cursorColor;
 QColor  MScore::defaultColor;
 
 bool    MScore::noteInputOctaveTendencyIsTopNote;
+bool    MScore::disableVerticalMouseDragOfNotes;
 
 QColor  MScore::layoutBreakColor;
 QColor  MScore::frameMarginColor;
