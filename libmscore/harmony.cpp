fdab39679ea86ac273824f88b3a2294dc5eabfa7
Jesse Greene <worldwideweary@aol.com>
2023-10-18 20:57:08 -0700

    [Command] PolyChord facilitation by way of CTRL+ENTER during Harmony Text Entry Style: Center and underlined with a hidden frame to allow underlines to be larger if needed by margin increase ...making use of the custom underline (entire bounding-box) mechanism for harmony in earlier commit + Also allow for applying harmony when harmony is selected instead of requiring valid chord-rest

--- libmscore/harmony.cpp
+++ libmscore/harmony.cpp
@@ -1520,7 +1520,6 @@ void Harmony::draw(QPainter* painter) const
 #endif
             }
 
-      // Want consistent-base underline (esp for polychord look)
       if (underline()) {
             auto lineWidth = fontMetrics().lineWidth();
             lineWidth *= 3;
