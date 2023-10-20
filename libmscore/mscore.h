12b9989cd7aeefe08b97f98b948809f434404cdd
Jesse Greene <worldwideweary@aol.com>
2023-10-20 00:28:56 -0700

    [Fingering Text] Updates: (Shift)+Tab moves by measure [more...]
    + (Shift)+CTRL+Space skips entire vertical tick (chord/voice) forward/backward
      - DETRIMENT: [CTRL+SPACE] won't insert an actual [space] (use Special Character list instead)
                   but this is super-rare for 1-5 fingering mode
      - Function: Always start at top of chord even when going backwards when skipping vertical ticks,
                  Also starts at voice one, for now, so it isn't a means to skip voices of the same tick.
    
    + Ensure Top to Bottom voicings during layout (Voice-1 through Voice-4) when switching above/below positions
        This allows for voice-2 voices to "tuck" under voice-1 when "above" setting is applied instead of being on top
        (which is stupid)
    
    +  Allow flipping [x] above/below while editing fingering-text easily
      - DETRIMENT Can't insert "x" character (which doesn't make sense anyways)
      - Note: a manual [x] alteration during entry will 'stick' through entry until a manual forward (Space/Tab/Ctrl-Space) occurs
    
    + Automatically move forward when using NUM-PAD, whereas QWERTY 1-5 will perform as older-style (useful for elision entry)
      - No more space pressing so much when continual entry.
      - Because of this, "Edit Flipping" needs to be performed pre-number entry, which is fine, but requires a change in habit
    
    + Tighten spacing slightly from 25% height of text to 15%
    
    + TODO ? Make line spacing an advanced preference

--- libmscore/mscore.h
+++ libmscore/mscore.h
@@ -339,6 +339,7 @@ class MScore {
 
       static bool noteInputOctaveTendencyIsTopNote;
       static bool noteInputOctaveUpwardFifth;
+      static bool fingerTextAutoForwardAlphaNumeric;
 
       static bool disableVerticalMouseDragOfNotes;
 
