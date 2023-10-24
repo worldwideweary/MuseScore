e46ee7fa39bc6589f93ecdaa37cb322931fbd750
Jesse Greene <worldwideweary@aol.com>
2023-10-24 15:27:25 -0700

    Advanced Preference: Note-Entry - Untoggle augmentation when toggling/inserting rhythms via Rhythm Entry Mode [true/false] "ui/score/NoteEntry/rhythmMode/retainAugmentation"

--- libmscore/score.cpp
+++ libmscore/score.cpp
@@ -3245,6 +3245,9 @@ void Score::padToggle(Pad p, const EditData& ed)
                               }
                         setNoteRest(_is.segment(), _is.track(), nval, _is.duration().fraction(), stemDirection);
                         _is.moveToNextInputPos();
+                        if (!MScore::retainAugmentationInRhythmEntry) {
+                              _is.setDots(0);
+                              }
                         }
                   else
                         _is.setRest(false);
