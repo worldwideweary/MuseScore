4b0056d85811de2330596f3d46a862a6ca800a93
Jesse Greene <worldwideweary@aol.com>
2023-11-07 00:27:07 -0800

    Layout - Slurs: (Adv. Preferences) Extra shoulder height changes for "hack" customization

--- mscore/preferences.cpp
+++ mscore/preferences.cpp
@@ -295,6 +295,8 @@ void Preferences::init(bool storeInMemoryOnly)
             {PREF_UI_SCORE_VOICE4_COLOR,                           new ColorPreference(QColor(0xC31989))},
             {PREF_UI_SCORE_CURSOR_COLOR,                           new ColorPreference(QColor(0x0065BF))},
             {PREF_UI_SCORE_BRACKET_MULTIPLIER,                     new DoublePreference(0.25)},
+            {PREF_UI_SCORE_SLURS_MAX_SHOULDER_EXTRA,               new DoublePreference(1.8)}, // Use 1.0 for 3.6.2 equivalent
+            {PREF_UI_SCORE_SLURS_MIN_SHOULDER_EXTRA,               new DoublePreference(1.2)},
             {PREF_UI_THEME_ICONWIDTH,                              new IntPreference(28, false)},
             {PREF_UI_THEME_ICONHEIGHT,                             new IntPreference(24, false)},
             {PREF_UI_THEME_FONTFAMILY,                             new StringPreference(QApplication::font().family(), false) },
