f47251848c3b4229cbceaf1ae6a354a7827edf50
Jesse Greene <worldwideweary@aol.com>
2023-12-25 23:59:43 -0800

    Efficiency Change: slow-down from applying accidental [more...] 	+ As score-size increases, accidental placement when more than one in a measure (or naturals) have slow-down, along with 	tie-into another measure. Why? There was a re-layouting from beginning of score occurring because of invalid ticks 	starting at 0/1 fractions ....
    	+ Reset slur was retriggering full layout when altered terminal points

--- libmscore/element.cpp
+++ libmscore/element.cpp
@@ -2234,7 +2234,7 @@ bool Element::isUserModified() const
 void Element::triggerLayout() const
       {
       if (parent())
-            score()->setLayout(tick(), staffIdx(), this);
+            score()->setLayout(parent()->tick(), staffIdx(), this);
       }
 
 //----------------------------------------------------------------------
