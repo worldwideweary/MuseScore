d5a374c72ef35d9ea8522194373654578672762a
novabond <worldwideweary@aol.com>
2024-03-08 02:58:36 -0800

    Scoreview: Current playing/selected system will show as first system with uniform spacing of upper-border

--- mscore/musescore.cpp
+++ mscore/musescore.cpp
@@ -471,6 +471,8 @@ void updateExternalValuesFromPreferences() {
       MScore::hoverColorEnabled = preferences.getBool(PREF_SCORE_HOVER_COLOR_ENABLE);
       MScore::pedalEventsMinTicks = preferences.getInt(PREF_IO_MIDI_PEDAL_EVENTS_MIN_TICKS);
       MScore::fadeFocus = preferences.getBool(PREF_UI_SCORE_FADE_FOCUS);
+      MScore::currentSystemAlwaysTop = preferences.getBool(PREF_UI_SCORE_CURRENT_SYS_ON_TOP);
+            
       MScore::layoutBreakColor = preferences.getColor(PREF_UI_SCORE_LAYOUTBREAKCOLOR);
       MScore::frameMarginColor = preferences.getColor(PREF_UI_SCORE_FRAMEMARGINCOLOR);
       MScore::setVerticalOrientation(preferences.getBool(PREF_UI_CANVAS_SCROLL_VERTICALORIENTATION));
