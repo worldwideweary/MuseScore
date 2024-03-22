f627011cb0b179defeefee2f3eec090eefbda9b4
novabond <worldwideweary@aol.com>
2024-03-22 04:22:44 -0700

    [CMD] apply accidentals (non-toggle) allow to toggle-apply

--- libmscore/cmd.cpp
+++ libmscore/cmd.cpp
@@ -1872,8 +1872,10 @@ void Score::toggleAccidental(AccidentalType at, const EditData& ed)
 
 void Score::changeAccidental(AccidentalType idx)
       {
-      foreach(Note* note, selection().noteList())
+      for (auto note : selection().noteList()) {
+            idx = (note->accidentalType() != idx) ? idx : AccidentalType::NONE;
             changeAccidental(note, idx);
+            }
       }
 
 //---------------------------------------------------------
