3eed625e17447936d6b4dbf58a1ea7d57d78bf8c
novabond <worldwideweary@aol.com>
2024-03-31 11:10:54 -0700

    Efficiency: changeVoice no longer layouts the entire score - Plus, endCmd() already performs a range-based layout, it would seem

--- libmscore/score.cpp
+++ libmscore/score.cpp
@@ -5214,7 +5214,6 @@ void Score::changeVoice(int voice)
             selection().clear();
       for (Element* e : el)
             select(e, SelectType::ADD, -1);
-      setLayoutAll();
       endCmd();
       }
 
