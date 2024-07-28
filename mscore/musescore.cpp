b8e490120300bc2430b7529593b1272817a37ebf
J.G <worldwideweary@aol.com>
2024-07-28 13:09:09 +0000

    Navigation: Fix view update during Figure Bass (and others?) while keeping fix for undo/redo navigation flaw

--- mscore/musescore.cpp
+++ mscore/musescore.cpp
@@ -6270,7 +6270,7 @@ void MuseScore::endCmd(bool undoRedo)
                   selectionChanged(ss);
                   }
 
-            if (cv)
+            if (cv && !undoRedo)
                   cv->moveViewportToLastEdit();
 
             getAction("concert-pitch")->setChecked(cs->styleB(Sid::concertPitch));
