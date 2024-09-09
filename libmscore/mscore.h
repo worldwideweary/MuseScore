899c2527a48e0d3d7cf088d0c13de03a18b93844
J.G <worldwideweary@aol.com>
2024-09-09 10:33:28 +0000

    [Lasso] (Adv. Preference) Don't require shift-key to begin lasso dragging

--- libmscore/mscore.h
+++ libmscore/mscore.h
@@ -348,6 +348,7 @@ class MScore {
       static bool fingerTextAutoForwardAlphaNumeric;
 
       static bool disableVerticalMouseDragOfNotes;
+      static bool lassoWithoutShift;
 
       static bool palettesHideWhenApplied;
 
