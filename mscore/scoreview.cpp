4b4cdffa7a4574646743ae79234f2ed8afd1059c
J.G <worldwideweary@aol.com>
2024-10-01 04:12:34 +0000

    New Command: [Next/Previous Staff] without traversing voices.

--- mscore/scoreview.cpp
+++ mscore/scoreview.cpp
@@ -2534,6 +2534,8 @@ void ScoreView::cmd(const char* s)
               "prev-chord",
               "next-track",
               "prev-track",
+              "next-staff",
+              "prev-staff",
               "next-measure",
               "prev-measure",
               "next-system",
