5ea828ed5acc0864c4ce008d29307c1578390c68
J.G <worldwideweary@aol.com>
2024-10-08 00:28:34 +0000

    [Command: Backspace] Update: Delete entire current chord while in note-entry, or first move to a previous chord if a rest is selected

--- libmscore/edit.cpp
+++ libmscore/edit.cpp
@@ -1757,7 +1757,7 @@ void Score::deleteItem(Element* el)
                                           }
                                     }
                               }
-                        //select(rest, SelectType::SINGLE, 0);
+                        select(rest, SelectType::SINGLE, 0);
                         }
                   else  {
                         // remove segment if empty
