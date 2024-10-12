973d408d553d49e366652ef5bad2c7065a197918
J.G <worldwideweary@aol.com>
2024-10-12 12:10:50 +0000

    Note-Entry functions as stop during playback and brings into note entry at playback cursor position

--- mscore/events.cpp
+++ mscore/events.cpp
@@ -1147,7 +1147,8 @@ static const char* stateName(ViewState s)
 
 void ScoreView::seqStopped()
       {
-      changeState(ViewState::NORMAL);
+      if (state != ViewState::NORMAL && state != ViewState::NOTE_ENTRY)
+            changeState(ViewState::NORMAL);
       }
 
 //---------------------------------------------------------
