15a66c691a189f864b9a5a4c83b2d105f5e49ced
J.G <worldwideweary@aol.com>
2024-10-12 05:48:20 +0000

    Play Panel: set focus on Master Volume to easily change up/down

--- mscore/playpanel.cpp
+++ mscore/playpanel.cpp
@@ -170,6 +170,8 @@ void PlayPanel::showEvent(QShowEvent* e)
             activateWindow();
             setFocus();
             }
+      volSpinBox->setFocus();
+      volSpinBox->selectAll();
       }
 
 //---------------------------------------------------------
