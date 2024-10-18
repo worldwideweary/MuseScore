10c34013ab3163c7b96f499ee7842c6ebf698769
J.G <worldwideweary@aol.com>
2024-10-18 08:49:26 +0000

    Grip Nodes: Allow numeric 1-2-3-4 for start-drag-shoulder-end selection as alternative to tabbing cycle

--- libmscore/element.cpp
+++ libmscore/element.cpp
@@ -2013,6 +2013,16 @@ bool Element::prevGrip(EditData& ed) const
       }
 
 //---------------------------------------------------------
+//   setGrip
+//---------------------------------------------------------
+
+bool Element::setGrip(EditData& ed, Grip g) const
+      {
+      ed.curGrip = Grip(g);
+      return true;
+      }
+
+//---------------------------------------------------------
 //   isUserModified
 //    Check if this element was modified by user and
 //    therefore must be saved.
