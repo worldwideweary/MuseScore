96ad341e8a86793979396ed043f739883370739c
J.G <worldwideweary@aol.com>
2024-11-29 05:42:39 +0000

    Command: Pitch Up/Down - Change default behavior: Transpose up/down if range selection exists, including harmony if selected within range (not filtered)

--- libmscore/cmd.cpp
+++ libmscore/cmd.cpp
@@ -4384,6 +4384,7 @@ void Score::cmdRemoveEmptyTrailingMeasures()
 
 void Score::cmdPitchUp()
       {
+      bool isRange = selection().isRange();
       Element* el = selection().element();
       if (el && el->isLyrics())
             cmdMoveLyrics(toLyrics(el), Direction::UP);
@@ -4391,6 +4392,10 @@ void Score::cmdPitchUp()
             el->undoChangeProperty(Pid::OFFSET, el->offset() + QPointF(0.0, -MScore::nudgeStep * el->spatium()), PropertyFlags::UNSTYLED);
       else if (el && el->isRest())
             cmdMoveRest(toRest(el), Direction::UP);
+      else if (isRange) {
+            int up = 1;
+            transposeSemitone(up);
+            }
       else
             upDown(true, UpDownMode::CHROMATIC);
       }
@@ -4401,6 +4406,7 @@ void Score::cmdPitchUp()
 
 void Score::cmdPitchDown()
       {
+      bool isRange = selection().isRange();
       Element* el = selection().element();
       if (el && el->isLyrics())
             cmdMoveLyrics(toLyrics(el), Direction::DOWN);
@@ -4408,6 +4414,10 @@ void Score::cmdPitchDown()
             el->undoChangeProperty(Pid::OFFSET, el->offset() + QPointF(0.0, MScore::nudgeStep * el->spatium()), PropertyFlags::UNSTYLED);
       else if (el && el->isRest())
             cmdMoveRest(toRest(el), Direction::DOWN);
+      else if (isRange) {
+            int down = -1;
+            transposeSemitone(down);
+            }
       else
             upDown(false, UpDownMode::CHROMATIC);
       }
