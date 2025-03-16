ad22d93826e14014fc29cd9f186c11916b561333
J.G <worldwideweary@aol.com>
2025-03-16 20:17:31 +0000

    Interaction: Advanced Preference: Fade Score when not in focus (uses invisible elements color)

--- libmscore/mscore.h
+++ libmscore/mscore.h
@@ -368,6 +368,8 @@ class MScore {
       static bool disableMouseEntry;
       static int pedalEventsMinTicks;
 
+      static bool fadeFocus;
+
       static bool cursorMoveByBeat;
       static bool cursorMoveByMeasure;
       static bool cursorDrawnBehindStaff;
