e328f331a6c06b722d9e4c010c169068243346ba
J.G <worldwideweary@aol.com>
2025-03-18 17:27:51 +0000

    Global Color Override: Barline & Brackets

--- libmscore/mscore.cpp
+++ libmscore/mscore.cpp
@@ -98,6 +98,8 @@ QColor  MScore::lassoColor;
 bool    MScore::lassoBorderEnabled;
 QColor  MScore::gripsColor;
 
+QColor  MScore::overrideBarlinesColor;
+QColor  MScore::overrideBracketsColor;
 QColor  MScore::overrideStaffLinesColor;
 
 bool    MScore::cursorResetToStart;
