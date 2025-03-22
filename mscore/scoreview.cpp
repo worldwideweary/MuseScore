3c14d16d86f3f9ab3d53ab950742bea498dce5d9
J.G <worldwideweary@aol.com>
2025-03-22 10:54:16 +0000

    Fix: add interval upon rest

--- mscore/scoreview.cpp
+++ mscore/scoreview.cpp
@@ -3474,7 +3474,7 @@ void ScoreView::cmd(const char* s)
                         }
                   else  {
                         auto score      = cv->score();
-                        auto selection  = score->selection();
+                        auto& selection = score->selection();
                         auto& is        = score->inputState();
                         auto cr         = selection.currentCR();
 
