532dd059d564a6e8ebfbecc75993826ee10fa6b3
J.G <worldwideweary@aol.com>
2025-04-09 08:26:27 +0000

    Note Entry Status: Create preference to enable/disable

--- libmscore/mscore.cpp
+++ libmscore/mscore.cpp
@@ -133,6 +133,7 @@ bool    MScore::highlightLyrics;
 bool    MScore::honorEnPassantVisibility;
 
 QColor  MScore::noteEntryInformationColor;
+bool    MScore::noteEntryInformationEnabled;
 bool    MScore::noteInputOctaveTendencyIsTopNote;
 bool    MScore::noteInputOctaveUpwardFifth;
 bool    MScore::resetNoteEntryAtSystemOrCourtesy;
