16809592a47c2afec35c8109ba001370ba721406
novabond <worldwideweary@aol.com>
2025-04-23 22:34:57 -0700

    Fix: En passant visibility with Highlight More

--- mscore/scoreview.cpp
+++ mscore/scoreview.cpp
@@ -3080,6 +3080,7 @@ void ScoreView::cmd(const char* s)
                                           if (tlb->isTemporarilyShowing()) {
                                                 tlb->setTemporarilyShowing(false);
                                                 tlb->setVisible(false);
+                                                tlb->setSelected(false);
                                                 }
                                           }
                                     }
--- mscore/seq.cpp
+++ mscore/seq.cpp
@@ -2731,7 +2731,7 @@ void Seq::heartBeatTimeout()
       if (piano && piano->isVisible())
             piano->updateAllKeys();
 
-      MScore::highlightMore ? cv->update() : cv->update(cv->toPhysical(r));
+      cv->update();
       }
 
 //---------------------------------------------------------
