3fb91eb93bf95af699e67065edd8db62a8e63446
J.G <worldwideweary@aol.com>
2025-04-30 00:36:37 +0000

    Lasso Annotation: Advanced Preference

--- libmscore/mscore.cpp
+++ libmscore/mscore.cpp
@@ -126,6 +126,7 @@ bool    MScore::fingerTextAutoForwardAlphaNumeric;
 
 bool    MScore::disableVerticalMouseDragOfNotes;
 bool    MScore::lassoWithoutShift;
+bool    MScore::lassoAnnotations;
 
 bool    MScore::palettesHideWhenApplied;
 
