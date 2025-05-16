3d3499c1b6c11f0b11d6791e4053ce93eb024b4f
J.G <worldwideweary@aol.com>
2025-05-16 20:20:21 +0000

    Fix: Navigate to (next/prev system) in Note Entry Mode needs to lose Note Entry to omit undefined behavior

--- libmscore/cmd.cpp
+++ libmscore/cmd.cpp
@@ -2902,7 +2902,7 @@ Element* Score::move(const QString& cmd)
 
             if (!el) el = cmdNextPrevSystem(cr, next);
 
-            if (noteEntryMode())
+            if (noteEntryMode() && !isDestBox)
                   _is.moveInputPos(el);
             }
       else if (cmd == "next-frame") {
