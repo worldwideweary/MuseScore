ebac2efedadd4c035dd58eff071e09f23fa7ff6e
J.G <worldwideweary@aol.com>
2025-06-04 09:39:41 +0000

    Aside: Script Recorder nullified

--- mscore/musescore.cpp
+++ mscore/musescore.cpp
@@ -7254,10 +7254,6 @@ Timeline* MuseScore::timeline() const
 
 ScriptRecorder* MuseScore::getScriptRecorder()
       {
-#ifdef MSCORE_UNSTABLE
-      if (scriptRecorder)
-            return &scriptRecorder->scriptRecorder();
-#endif
       return nullptr;
       }
 
