28cce93bcf16d800995c88b2e545ae0e54207da4
J.G <worldwideweary@aol.com>
2025-06-08 08:36:00 +0000

    Inspector: Fretboard Diagram will trigger selection update so that OSK gets live update

--- libmscore/fret.cpp
+++ libmscore/fret.cpp
@@ -262,6 +262,7 @@ void FretDiagram::setStrings(int n)
             }
 
       _strings = n;
+      score()->setSelectionChanged(true);
       }
 
 //---------------------------------------------------------
@@ -1384,6 +1385,7 @@ void FretDiagram::setDot(int string, int fret, bool add /*= false*/, FretDotType
             if (!add)
                   setMarker(string, FretMarkerType::NONE);
             }
+      score()->setSelectionChanged(true);
       }
 
 //---------------------------------------------------------
@@ -1397,6 +1399,7 @@ void FretDiagram::setMarker(int string, FretMarkerType mtype)
       if (string >= 0 && string < _strings) {
             _markers[string] = FretItem::Marker(mtype);
             }
+      score()->setSelectionChanged(true);
       }
 
 //---------------------------------------------------------
@@ -1594,6 +1597,7 @@ void FretDiagram::clear()
       _barres.clear();
       _dots.clear();
       _markers.clear();
+      score()->setSelectionChanged(true);
       }
 
 //---------------------------------------------------------
@@ -1606,6 +1610,26 @@ void FretDiagram::undoFretClear()
             FretDiagram* fd = toFretDiagram(e);
             fd->score()->undo(new FretClear(fd));
             }
+      }
+
+//---------------------------------------------------------
+//   setFretOffset
+//---------------------------------------------------------
+
+void FretDiagram::setFretOffset(int val)
+      {
+      _fretOffset = val;
+      score()->setSelectionChanged(true);
+      }
+
+//---------------------------------------------------------
+//   setMaxFrets
+//---------------------------------------------------------
+
+void FretDiagram::setMaxFrets(int val)
+      {
+      _maxFrets = val;
+      score()->setSelectionChanged(true);
       }
 
 //---------------------------------------------------------
