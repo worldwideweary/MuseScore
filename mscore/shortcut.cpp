396da2e159beb6394268a656dc59d5b0a133d673
J.G <worldwideweary@aol.com>
2025-06-23 17:06:15 +0000

    New Command: Seq::nextSystem

--- mscore/shortcut.cpp
+++ mscore/shortcut.cpp
@@ -3366,6 +3366,16 @@ Shortcut Shortcut::_sc[] = {
       {
          MsWidget::MAIN_WINDOW,
          STATE_PLAY,
+         "play-prev-system",
+         QT_TRANSLATE_NOOP("action","Play Previous System"),
+         QT_TRANSLATE_NOOP("action","Play previous system"),
+         0,
+         Icons::Invalid_ICON,
+         Qt::ApplicationShortcut
+      },
+      {
+         MsWidget::MAIN_WINDOW,
+         STATE_PLAY,
          "play-next-chord",
          QT_TRANSLATE_NOOP("action","Play Next Chord"),
          QT_TRANSLATE_NOOP("action","Play next chord"),
@@ -3383,6 +3393,16 @@ Shortcut Shortcut::_sc[] = {
          Icons::Invalid_ICON,
          Qt::ApplicationShortcut
          },
+      {
+         MsWidget::MAIN_WINDOW,
+         STATE_PLAY,
+         "play-next-system",
+         QT_TRANSLATE_NOOP("action","Play Next System"),
+         QT_TRANSLATE_NOOP("action","Play next system"),
+         0,
+         Icons::Invalid_ICON,
+         Qt::ApplicationShortcut
+      },
       {
          MsWidget::SCORE_TAB,
          STATE_PLAY,
