b2362ef91a28327cc1f146bbda61907227117620
J.G <worldwideweary@aol.com>
2025-07-14 02:21:40 +0000

    Command: Backspace/Delete to work on Segments during edit mode Also allow backspace to work as regular delete when not in note-entry mode (range selection)

--- mscore/shortcut.cpp
+++ mscore/shortcut.cpp
@@ -1548,7 +1548,7 @@ Shortcut Shortcut::_sc[] = {
          },
       {
          MsWidget::SCORE_TAB,
-         STATE_NORMAL | STATE_NOTE_ENTRY,
+         STATE_NORMAL | STATE_NOTE_ENTRY | STATE_EDIT,
          "delete",
          QT_TRANSLATE_NOOP("action","Delete"),
          QT_TRANSLATE_NOOP("action","Delete"),
@@ -3826,7 +3826,7 @@ Shortcut Shortcut::_sc[] = {
          },
       {                     // mapped to undo in note entry mode
          MsWidget::SCORE_TAB,
-         STATE_NORMAL | STATE_NOTE_ENTRY,
+         STATE_NORMAL | STATE_NOTE_ENTRY | STATE_EDIT,
          "backspace",
          QT_TRANSLATE_NOOP("action","Backspace"),
 //         0,
