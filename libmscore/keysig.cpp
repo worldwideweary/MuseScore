e45bfac2b463f8e9b344b7b9b19a5e0f7d302d12
J.G <worldwideweary@aol.com>
2025-07-21 15:18:17 +0000

    Layout: Key Signatures - Omit showing C Major Naturals as courtesy if Section Break exists on previous system

--- libmscore/keysig.cpp
+++ libmscore/keysig.cpp
@@ -159,7 +159,7 @@ void KeySig::layout()
       if (measure() && measure()->system() && measure()->isFirstInSystem()
           && prevMeasure && prevMeasure->findSegment(SegmentType::KeySigAnnounce, tick())
           && !segment()->isKeySigAnnounceType())
-            naturalsOn = (keySigEvent().key() == Key::C);
+            naturalsOn = (Key::C == keySigEvent().key() && !prevMeasure->sectionBreak());
       if (track() == -1)
             naturalsOn = false;
 
