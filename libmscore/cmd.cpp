51b47de28b840a1757ce19c824d1e635fae23384
J.G <worldwideweary@aol.com>
2025-07-22 04:55:30 +0000

    Score Commands: force update scoreview after completion. Specificall for delete/split/join measures

--- libmscore/cmd.cpp
+++ libmscore/cmd.cpp
@@ -5922,6 +5922,12 @@ void Score::cmd(const QAction* a, EditData& ed)
                   startCmd();
                   c.cmd(this, ed);
                   endCmd();
+
+                  if (const auto se = selection().element()) {
+                        for(MuseScoreView* v : qAsConst(viewer))
+                              v->adjustCanvasPosition(se, false);
+                        }
+
                   return;
                   }
             }
