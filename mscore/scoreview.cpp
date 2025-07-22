11eee4213d57cf7066d9474844fc53df65f94240
J.G <worldwideweary@aol.com>
2025-07-22 07:17:45 +0000

    Note Entry: Re-enter appropriately at beginning/end of slur after editing it

--- mscore/scoreview.cpp
+++ mscore/scoreview.cpp
@@ -4552,7 +4552,8 @@ void ScoreView::startNoteEntry()
                   }
             }
 
-      Element* el = _score->selection().element();
+      Element* oel = _score->selection().element();
+      Element* el = oel;
       if (!el)
             el = _score->selection().firstChordRest();
       if (el == 0 || (el->type() != ElementType::CHORD && el->type() != ElementType::REST && el->type() != ElementType::NOTE)) {
@@ -4579,6 +4580,17 @@ void ScoreView::startNoteEntry()
             if (note == 0)
                   note = c->upNote();
             el = note;
+
+            if (oel) {
+                  if (oel->isSlurSegment()) {
+                        auto ss = toSlurSegment(oel);
+                        auto s = ss->slur();
+                        el = (ss == s->backSegment()) ? s->endElement() : s->startElement();
+                        if (el->isChord())
+                              el = toChord(el)->upNote();
+                        }
+                  }
+
             defaultDuration = c->durationType();
             }
       else if (el->type() == ElementType::REST) {
