99ce2ab7ca01eb903e91c6218cff9a1540a82fbf
J.G <worldwideweary@aol.com>
2025-07-27 03:24:23 +0000

    Note Entry: Ensure no full-range layout occurs on activating (can happen on first activation instance after loading a score)

--- mscore/scoreview.cpp
+++ mscore/scoreview.cpp
@@ -4613,7 +4613,7 @@ void ScoreView::startNoteEntry()
       setMouseTracking(true);
       if (!MScore::disableMouseEntry)
             shadowNote->setVisible(true);
-      _score->setUpdateAll();
+      _score->setUpdateAllNoLayout();
       _score->update();
 
       Staff* staff = _score->staff(is.track() / VOICES);
