5db7af890b2cf8362b8e1c5a4c98a843cf3ce4e0
J.G <worldwideweary@aol.com>
2025-09-03 02:00:16 +0000

    Mouse Hover: Fix accidentally forced focus when no longer highlighting on an element

--- mscore/scoreview.cpp
+++ mscore/scoreview.cpp
@@ -2327,7 +2327,6 @@ void ScoreView::updateHover(const QPointF& position)
             else if (pastHover) {
                   view = pastHover->canvasBoundingRect();
                   setDropTarget(nullptr);
-                  ScoreAccessibility::instance()->updateAccessibilityInfo();
                   }
             const int margin = 2;
             update(toPhysical(view).adjusted(-margin, -margin, +margin, +margin));
