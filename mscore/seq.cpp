291b2af665142cb4cf1d7365076a93c02931a189
J.G. <worldwideweary@aol.com>
2025-10-17 00:57:26 +0000

    Sequencer: Aside: increase NDEBUG threshold of stop/wait

--- mscore/seq.cpp
+++ mscore/seq.cpp
@@ -505,7 +505,7 @@ void Seq::stopWait()
             sleep.wait(&mutex, 100);
             mutex.unlock();
 #ifndef NDEBUG
-            Q_ASSERT(++idx <= 10);
+            Q_ASSERT(++idx <= 100);
 #endif
             }
       }
