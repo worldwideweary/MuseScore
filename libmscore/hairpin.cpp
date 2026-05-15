1bbcceb09a2a6cf14c70092cd1a6367a0178027d
J.G. <worldwideweary@aol.com>
2026-05-15 08:02:55 +0000

    Hairpin: New style option (experiment)  providing a "piano" based hairpin, changing default start/end positionings to be at stems
    Note: When a particular voice is given an offset during layout with
    multi-voicing, the offset doesn't seem to be stored
    as an attribute of a chordrest or chord/note... so it doesn't seem
    accessible when performing layout of the hairpin...

--- libmscore/hairpin.cpp
+++ libmscore/hairpin.cpp
@@ -51,6 +51,7 @@ static const ElementStyle hairpinStyle {
       { Sid::hairpinPlacement,                   Pid::PLACEMENT                  },
       { Sid::hairpinPosBelow,                    Pid::OFFSET                     },
       { Sid::hairpinLineStyle,                   Pid::LINE_STYLE                 },
+      { Sid::hairpinPianoStyle,                  Pid::HAIRPIN_PIANO_STYLE        },
       };
 
 //---------------------------------------------------------
@@ -490,6 +491,7 @@ Element* HairpinSegment::propertyDelegate(Pid pid)
             case Pid::DYNAMIC_RANGE:
             case Pid::HAIRPIN_CIRCLEDTIP:
             case Pid::HAIRPIN_CONT_HEIGHT:
+            case Pid::HAIRPIN_PIANO_STYLE:
             case Pid::HAIRPIN_HEIGHT:
             case Pid::HAIRPIN_TYPE:
             case Pid::LINE_STYLE:
@@ -535,6 +537,8 @@ Sid HairpinSegment::getPropertyStyle(Pid pid) const
                   break;
             case Pid::LINE_STYLE:
                   return hairpin()->isLineType() ? Sid::hairpinLineLineStyle : Sid::hairpinLineStyle;
+            case Pid::HAIRPIN_PIANO_STYLE:
+                  return Sid::hairpinPianoStyle;
             default:
                   break;
             }
@@ -570,6 +574,8 @@ Sid Hairpin::getPropertyStyle(Pid pid) const
                   break;
             case Pid::LINE_STYLE:
                   return isLineType() ? Sid::hairpinLineLineStyle : Sid::hairpinLineStyle;
+            case Pid::HAIRPIN_PIANO_STYLE:
+                  return Sid::hairpinPianoStyle;
             default:
                   break;
             }
@@ -598,6 +604,7 @@ Hairpin::Hairpin(Score* s)
       _dynRange              = Dynamic::Range::PART;
       _singleNoteDynamics    = true;
       _veloChangeMethod      = ChangeMethod::NORMAL;
+      _hairpinPianoStyle     = true;
       }
 
 //---------------------------------------------------------
@@ -680,6 +687,7 @@ void Hairpin::write(XmlWriter& xml) const
       writeProperty(xml, Pid::HAIRPIN_HEIGHT);
       writeProperty(xml, Pid::HAIRPIN_CONT_HEIGHT);
       writeProperty(xml, Pid::PLACEMENT);
+      writeProperty(xml, Pid::HAIRPIN_PIANO_STYLE);
 
       TextLineBase::writeProperties(xml);
       xml.etag();
@@ -729,6 +737,8 @@ void Hairpin::read(XmlReader& e)
                   _singleNoteDynamics = e.readBool();
             else if (tag == "veloChangeMethod")
                   _veloChangeMethod = ChangeMap::nameToChangeMethod(e.readElementText());
+            else if (tag == "hairpinPianoStyle")
+                  _hairpinPianoStyle = e.readBool();
             else if (!TextLineBase::readProperties(e))
                   e.unknown();
             }
@@ -758,6 +768,8 @@ QVariant Hairpin::getProperty(Pid id) const
                   return _singleNoteDynamics;
             case Pid::VELO_CHANGE_METHOD:
                   return int(_veloChangeMethod);
+            case Pid::HAIRPIN_PIANO_STYLE:
+                  return _hairpinPianoStyle;
             default:
                   return TextLineBase::getProperty(id);
             }
@@ -793,6 +805,9 @@ bool Hairpin::setProperty(Pid id, const QVariant& v)
                   break;
             case Pid::VELO_CHANGE_METHOD:
                   _veloChangeMethod = ChangeMethod(v.toInt());
+                  break;
+            case Pid::HAIRPIN_PIANO_STYLE:
+                  _hairpinPianoStyle = v.toBool();
                   break;
             default:
                   return TextLineBase::setProperty(id, v);
@@ -869,6 +884,8 @@ QVariant Hairpin::propertyDefault(Pid id) const
 
             case Pid::PLACEMENT:
                   return score()->styleV(Sid::hairpinPlacement);
+            case Pid::HAIRPIN_PIANO_STYLE:
+                  return true;
 
             default:
                   return TextLineBase::propertyDefault(id);
