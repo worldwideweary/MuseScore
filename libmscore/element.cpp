5fe247d8a1232457bc32744988ae1a96e7d9bc26
J.G. <worldwideweary@aol.com>
2026-05-15 08:04:32 +0000

    Layout: Allow to reverse hook flags (style option)

--- libmscore/element.cpp
+++ libmscore/element.cpp
@@ -1824,6 +1824,11 @@ void Element::undoSetVisible(bool v)
 //   drawSymbol
 //---------------------------------------------------------
 
+void Element::drawSymbolReversed(SymId id, QPainter* p, const QPointF& o, qreal scale) const
+      {
+      score()->scoreFont()->drawReversed(id, p, magS() * scale, o);
+      }
+
 void Element::drawSymbol(SymId id, QPainter* p, const QPointF& o, qreal scale) const
       {
       score()->scoreFont()->draw(id, p, magS() * scale, o);
