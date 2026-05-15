42c2610cb4eb60a13d5011c7562cc9214190d2ef
J.G. <worldwideweary@aol.com>
2026-05-15 07:11:03 +0000

    [Clear excess debug output for empty commands]
    [Piano Tutor] Rid debug messages about MIDI input etc

--- libmscore/cmd.cpp
+++ libmscore/cmd.cpp
@@ -5048,7 +5048,8 @@ void Score::cmd(const QAction* a, EditData& ed)
                   return;
                   }
             }
-      qDebug("unknown cmd <%s>", qPrintable(cmd));
+      if (cmd != "")
+            qDebug("unknown cmd <%s>", qPrintable(cmd));
       }
 
 
