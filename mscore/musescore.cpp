6c79dc73def26fbf541882c262fef4d0389c10c8
J.G. <worldwideweary@aol.com>
2026-05-28 18:52:35 +0000

    Search Functionality: Enter/Return required to perform search.
    Arrows or Ctrl Modifier searches but keeps focus on search box

--- mscore/musescore.cpp
+++ mscore/musescore.cpp
@@ -7743,10 +7743,8 @@ void MuseScore::endSearch()
       {
       if (_searchDialog)
             _searchDialog->hide();
-      if (searchCombo) {
-            auto txt = searchCombo->currentText();
-            searchCombo->editTextChanged(txt);
-            }
+      if (searchCombo)
+            searchCombo->performSearch();
       if (cv)
             cv->setFocus();
       }
