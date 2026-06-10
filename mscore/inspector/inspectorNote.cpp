4fd1567138d592b6c0b761d162520ee22a6fc337
J.G. <worldwideweary@aol.com>
2026-06-10 23:37:17 +0000

    New Branch: Expose OnTime & Length of Notes in the Inspector
    as with the piano-roll editor, these are in the same unit-size. 100% is
    equivalent to 1000 in the full sense. As it is, just as with the PRE,
    playing the score is required to get the instrument definition's full
    length: e.g. piano won't show 950 until playback, but rather 1000.

--- mscore/inspector/inspectorNote.cpp
+++ mscore/inspector/inspectorNote.cpp
@@ -105,6 +105,8 @@ InspectorNote::InspectorNote(QWidget* parent)
             { Pid::TUNING,         0, n.tuning,        n.resetTuning        },
             { Pid::VELO_TYPE,      0, n.velocityType,  n.resetVelocityType  },
             { Pid::VELO_OFFSET,    0, n.velocity,      n.resetVelocity      },
+            { Pid::GATE_TIME,      0, n.length,        n.resetLength        },
+            { Pid::ON_TIME,        0, n.onTime,        n.resetOnTime        },
             { Pid::FIXED,          0, n.fixed,         n.resetFixed         },
             { Pid::FIXED_LINE,     0, n.fixedLine,     n.resetFixedLine     },
 
@@ -133,6 +135,9 @@ InspectorNote::InspectorNote(QWidget* parent)
       connect(n.stem,     SIGNAL(clicked()),     SLOT(stemClicked()));
       connect(n.beam,     SIGNAL(clicked()),     SLOT(beamClicked()));
       connect(n.tuplet,   SIGNAL(clicked()),     SLOT(tupletClicked()));
+
+      connect(n.resetOnTime, &ResetButton::resetClicked, this, &InspectorNote::resetOnTimeClicked);
+      connect(n.resetLength, &ResetButton::resetClicked, this, &InspectorNote::resetOnTimeClicked);
       }
 
 //---------------------------------------------------------
@@ -173,6 +178,20 @@ void InspectorNote::setElement()
             n.fixedLine->setEnabled(false);
       if (!n.play->isChecked())
             n.playWidget->setVisible(false);
+      }
+
+//---------------------------------------------------------
+//   valueChanged
+//---------------------------------------------------------
+
+void InspectorNote::valueChanged(int idx)
+      {
+      // Update Score/PlayEvents
+      InspectorElementBase::valueChanged(idx);
+      if (auto note = toNote(inspector->element())) {
+            auto score = note->score();
+            score->createPlayEvents(note->chord());
+            }
       }
 
 //---------------------------------------------------------
@@ -325,6 +344,30 @@ void InspectorNote::tupletClicked()
       if (tuplet) {
             note->score()->select(tuplet);
             note->score()->update();
+            inspector->update();
+            }
+      }
+
+//---------------------------------------------------------
+//   resetOnTimeClicked
+//---------------------------------------------------------
+
+void InspectorNote::resetOnTimeClicked()
+      {
+      if (auto note = toNote(inspector->element())) {
+            note->resetOntime();
+            inspector->update();
+            }
+      }
+
+//---------------------------------------------------------
+//   resetLengthClicked
+//---------------------------------------------------------
+
+void InspectorNote::resetLengthClicked()
+      {
+      if (auto note = toNote(inspector->element())) {
+            note->resetLength();
             inspector->update();
             }
       }
