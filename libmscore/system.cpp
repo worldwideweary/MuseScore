4747b762eaf8b23702b44400141b6b4a61019967
J.G. <worldwideweary@aol.com>
2026-06-15 00:51:08 +0000

    Fix Layout: Slur with cross-staff and two-pass system

--- libmscore/system.cpp
+++ libmscore/system.cpp
@@ -859,25 +859,8 @@ void System::layout2()
 
       //---------------------------------------------------
       //  layout cross-staff slurs and ties
+      //  No longer needed due to two-pass system
       //---------------------------------------------------
-
-      Fraction stick = measures().front()->tick();
-      Fraction etick = measures().back()->endTick();
-      auto spanners = score()->spannerMap().findOverlapping(stick.ticks(), etick.ticks());
-
-      std::vector<Spanner*> spanner;
-      for (auto interval : spanners) {
-            Spanner* sp = interval.value;
-            if (sp->tick() < etick && sp->tick2() >= stick) {
-                  if (sp->isSlur()) {
-                        ChordRest* scr = sp->startCR();
-                        ChordRest* ecr = sp->endCR();
-                        int idx = sp->vStaffIdx();
-                        if (scr && ecr && (scr->vStaffIdx() != idx || ecr->vStaffIdx() != idx))
-                              sp->layoutSystem(this);
-                        }
-                  }
-            }
 
       }
 
