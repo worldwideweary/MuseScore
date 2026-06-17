3d7c095f96d9cd5abacb18851f046aee931e9459
J.G. <worldwideweary@aol.com>
2026-06-17 07:14:28 +0000

    Fix: Compiler Warning

--- mscore/scoreview.cpp
+++ mscore/scoreview.cpp
@@ -5511,7 +5511,7 @@ void ScoreView::adjustCanvasPosition(const Element* el, bool playBack, int staff
             return;
             }
 
-      const MeasureBase* m;
+      const MeasureBase* m {nullptr};
       if (!el)
             return;
       else if (el->type() == ElementType::NOTE)
