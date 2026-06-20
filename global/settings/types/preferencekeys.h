5ad461b1a6fffcf73b668c2cfd7a73a6906b4d1c
J.G. <worldwideweary@aol.com>
2026-06-20 04:50:34 +0000

    Advanced Preference for resetting audio when starting playback. Off by default
    Update:
    6e979b36e5 ("Sequencer: Reset Audio when starting playback now with
    faster results. Purpose is to not need to restart manually when a midi
    device has powered off and powered back on again", 2025-10-08)
    
    Made this based on advanced preference, as it can give an undesirable
    1-2 second wait-period while starting playback

--- global/settings/types/preferencekeys.h
+++ global/settings/types/preferencekeys.h
@@ -48,6 +48,7 @@
 #define PREF_APP_PLAYBACK_SPEEDINCREMENT                    "application/playback/speedIncrement"
 #define PREF_APP_PLAYBACK_LOOPTOSELECTIONONPLAY             "application/playback/setLoopToSelectionOnPlay"
 #define PREF_APP_PLAYBACK_DEFAULT_MASTER_VOLUME             "application/playback/defaultMasterVolume"
+#define PREF_APP_PLAYBACK_RESTART_MIDI_AUTOMATICALLY        "application/playback/devices/restartOnPlay"
 #define PREF_APP_USESINGLEPALETTE                           "application/useSinglePalette"
 #define PREF_APP_PALETTESCALE                               "application/paletteScale"
 #define PREF_APP_STARTUP_FIRSTSTART                         "application/startup/firstStart"
