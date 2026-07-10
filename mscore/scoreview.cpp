3a38a2f6569cf0236623bc6753f97aa511267ea4
J.G. <worldwideweary@aol.com>
2026-07-10 01:52:03 +0000

    Lasso: slight change regarding selection of notes

--- mscore/scoreview.cpp
+++ mscore/scoreview.cpp
@@ -5072,6 +5072,10 @@ void ScoreView::doDragLasso(QMouseEvent* ev)
       r = _matrix.mapRect(_lassoRect);
       QSize sz(r.size().toSize());
       mscore->statusBar()->showMessage(QString("%1 x %2").arg(sz.width()).arg(sz.height()), 3000);
+
+      if (!MScore::lassoAnnotations)
+            _score->lassoSelect(lasso->bbox());
+
       _score->addRefresh(lasso->canvasBoundingRect());
       _score->update();
       }
@@ -5086,7 +5090,10 @@ void ScoreView::endLasso()
             (void) currentModifiers;
 
       keyMods = Qt::NoModifier;
-      _score->lassoSelect(lasso->bbox());
+
+      if (!MScore::lassoAnnotations)
+            _score->lassoSelect(lasso->bbox());
+
       _score->addRefresh(lasso->canvasBoundingRect());
 
       if (MScore::lassoAnnotations) {
@@ -5119,7 +5126,8 @@ void ScoreView::endLasso()
 
       dropTarget = nullptr;
       lasso->setbbox(QRectF());
-      MScore::lassoAnnotations ? _score->selection().clear() : _score->lassoSelectEnd();
+      if (!MScore::lassoAnnotations)
+            _score->lassoSelectEnd();
       _score->update();
       mscore->endCmd();
       }
