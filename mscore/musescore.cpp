71b93e88b14846d9fd29bc59bd5f9539533621f1
J.G. <worldwideweary@aol.com>
2026-08-24 07:21:20 +0000

    PRE: Update to active score automatically Considers the first staff to be the 'originating' edit staff

--- mscore/musescore.cpp
+++ mscore/musescore.cpp
@@ -2838,6 +2838,11 @@ void MuseScore::setCurrentScoreView(ScoreView* view)
             }
       ScoreAccessibility::instance()->updateAccessibilityInfo();
 
+      if (pianorollEditor) {
+            Staff* st = cs && !cs->staves().isEmpty() ? cs->staff(0) : nullptr;
+            pianorollEditor->setStaff(st);
+            }
+
       MasterScore* master = cs->masterScore();
       if (!scoreWasShown[master]) {
             scoreWasShown[master] = true;
