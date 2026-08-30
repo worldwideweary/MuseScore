8e9cd111b1111b33d4aa92f32e84ab276c641ae9
J.G. <worldwideweary@aol.com>
2026-08-30 04:24:59 +0000

    PRE: Efficiency updates to painting Some early returns to omit updates() on the Qt level, plus some drawing cullings etc

--- mscore/pianoroll/pianoroll.cpp
+++ mscore/pianoroll/pianoroll.cpp
@@ -853,9 +853,11 @@ PianorollEditor::PianorollEditor(QWidget* parent)
                           _playbackFollowBaseTick
                           + elapsed * _playbackFollowTicksPerSecond;
 
-                    if (preferences.getBool(PREF_UI_PIANOROLL_PLAYBACK_SHOW_CURSOR)) {
+                    const bool showPlaybackCursor =
+                          preferences.getBool(PREF_UI_PIANOROLL_PLAYBACK_SHOW_CURSOR);
+
+                    if (showPlaybackCursor) {
                           pianoView->setPlaybackLocatorTick(predictedTick);
-                          ruler->setPlaybackLocatorTick(predictedTick);
 
                           if (_showPianoLevels && pianoLevels)
                                 pianoLevels->setPlaybackLocatorTick(predictedTick);
@@ -876,6 +878,12 @@ PianorollEditor::PianorollEditor(QWidget* parent)
                     if (!_playbackFollowScrolling) {
                           if (!pianoView->playbackTickBeyondCenter(predictedTick)) {
                                 pianoView->ensurePlaybackTickVisible(predictedTick);
+
+                                if (showPlaybackCursor
+                                    && _orientation == PianoRollOrientation::HORIZONTAL) {
+                                    ruler->setPlaybackLocatorTick(predictedTick);
+                                    }
+
                                 return;
                                 }
 
@@ -886,8 +894,6 @@ PianorollEditor::PianorollEditor(QWidget* parent)
 
                     ruler->setPlaybackLocatorTick(predictedTick);
                     });
-
-
 
       connect(pianoView, &PianoView::onTimeDragged, this, [this](int value) {
             _previewOnTime = value;
@@ -916,7 +922,8 @@ PianorollEditor::PianorollEditor(QWidget* parent)
             });
 
       connect(pianoView, &PianoView::noteEventsChanged, this, [this]() {
-            pianoLevels->update();
+            if (_showPianoLevels && pianoLevels)
+                  pianoLevels->update();
             updateSelection();
             });
 
@@ -1309,9 +1316,8 @@ void PianorollEditor::setPianoLevelsVisible(bool visible)
 
       if (pianoLevels) {
             if (visible) {
-                  //
-                  // Bring the view back into sync when it becomes visible.
-                  //
+                  pianoLevels->updateNotes();
+
                   pianoLevels->setXpos(
                         pianoView->horizontalScrollBar()->value());
 
@@ -1971,7 +1977,10 @@ void PianorollEditor::selectionChanged()
 
 
       pianoView->scene()->update();
-      pianoLevels->update();
+
+      if (_showPianoLevels && pianoLevels)
+            pianoLevels->update();
+
       updateSelection();
       }
 
@@ -1989,7 +1998,9 @@ void PianorollEditor::changeSelection(SelState)
       // so no separate selection transfer is required.
       //
       pianoView->scene()->update();
-      pianoLevels->update();
+
+      if (_showPianoLevels && pianoLevels)
+            pianoLevels->update();
 
       updateSelection();
 
@@ -2113,7 +2124,8 @@ void PianorollEditor::updateVelocity(Note* note)
 
       velocity->setValue(value);
 
-      pianoLevels->update();
+      if (_showPianoLevels && pianoLevels)
+            pianoLevels->update();
       }
 
 //---------------------------------------------------------
@@ -2147,7 +2159,8 @@ void PianorollEditor::velocityChanged(int val)
             }
       _score->endCmd();
 
-      pianoLevels->update();
+      if (_showPianoLevels && pianoLevels)
+            pianoLevels->update();
       }
 
 //---------------------------------------------------------
@@ -2463,7 +2476,9 @@ void PianorollEditor::doUpdate()
             return;
             }
       pianoView->updateNotes();
-      pianoLevels->updateNotes();
+
+      if (_showPianoLevels && pianoLevels)
+            pianoLevels->updateNotes();
       }
 
 //---------------------------------------------------------
@@ -2526,7 +2541,9 @@ void PianorollEditor::applyPitchEdit()
       // Refresh PRE note geometry and the tweak controls immediately.
       //
       pianoView->updateNotes();
-      pianoLevels->updateNotes();
+      if (_showPianoLevels && pianoLevels)
+            pianoLevels->updateNotes();
+
       updateSelection();
       pianoView->ensureSelectionVisible();
       }
@@ -2541,7 +2558,7 @@ void PianorollEditor::redraw() const
             pianoView->viewport()->update();
       if (pianoKbd)
             pianoKbd->update();
-      if (pianoLevels)
+      if (pianoLevels && _showPianoLevels)
             pianoLevels->update();
       }
 
@@ -2597,7 +2614,7 @@ void PianorollEditor::posChanged(POS p, unsigned tick)
 
             ruler->update();
 
-            if (_showPianoLevels)
+            if (_showPianoLevels && pianoLevels)
                   pianoLevels->update();
 
             return;
@@ -2608,13 +2625,13 @@ void PianorollEditor::posChanged(POS p, unsigned tick)
       // horizontal-only.  Avoid the full scene update in
       // vertical mode.
       //
-      if (_orientation == PianoRollOrientation::HORIZONTAL)
+      if (_orientation == PianoRollOrientation::HORIZONTAL
+          && !_playbackFollowActive) {
             pianoView->moveLocator(int(p));
+            }
 
-      //
-      // PianoRuler is now visible in both orientations.
-      //
-      ruler->update();
+      if (!ruler->playbackLocatorActive())
+            ruler->update();
 
       if (waveView)
             waveView->moveLocator(int(p));
@@ -2648,7 +2665,9 @@ void PianorollEditor::onTimeChanged(int val)
       _score->endCmd();
 
       pianoView->updateNotes();
-      pianoLevels->updateNotes();
+
+      if (_showPianoLevels && pianoLevels)
+            pianoLevels->updateNotes();
       }
 
 //---------------------------------------------------------
@@ -2677,7 +2696,9 @@ void PianorollEditor::tickLenChanged(int val)
       _score->endCmd();
 
       pianoView->updateNotes();
-      pianoLevels->updateNotes();
+
+      if (_showPianoLevels && pianoLevels)
+            pianoLevels->updateNotes();
       }
 
 //---------------------------------------------------------
