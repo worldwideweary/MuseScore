ebe9ac86cc81ca50d2a729c5dd2f58a5404691ac
J.G. <worldwideweary@aol.com>
2026-09-02 22:02:21 +0000

    update workspace alternative tools thing

--- mscore/musescore.cpp
+++ mscore/musescore.cpp
@@ -991,7 +991,11 @@ void MuseScore::populateToggleOptionsMenu()
 
 void MuseScore::populateAlternativeOperations()
       {
+      if (!alternativeTools)
+            return;
+
       alternativeTools->clear();
+
       for (const auto s : _alternativeEntries) {
             if (!*s)
                   alternativeTools->addSeparator();
