ac774acc38b435716c15696ac45a3453c3e041c1
J.G. <worldwideweary@aol.com>
2026-09-07 09:15:09 +0000

    Piano Roll Editor: Fix slowdown of clicking note on huge scores

--- mscore/pianoroll/pianoview.cpp
+++ mscore/pianoroll/pianoview.cpp
@@ -5547,12 +5547,7 @@ void PianoView::clearNoteSelection()
       score->startCmd();
       score->selection().deselectAll();
 
-      for (MuseScoreView* view : score->getViewer())
-            view->updateAll();
-
       scene()->update();
-      score->setUpdateAll();
-      score->update();
       score->endCmd();
 
       emit selectionChanged();
@@ -5615,12 +5610,7 @@ void PianoView::selectItem(PianoItem* item, NoteSelectType selType)
                   selection.add(note);
             }
 
-      for (MuseScoreView* view : score->getViewer())
-            view->updateAll();
-
       scene()->update();
-      score->setUpdateAll();
-      score->update();
       score->endCmd();
 
       QList<PianoItem*> selectedItems = getSelectedItems();
@@ -5682,12 +5672,7 @@ void PianoView::selectNotes(int startTick, int endTick, int lowPitch, int highPi
                   selection.add(pi->note());
             }
 
-      for (MuseScoreView* view : score->getViewer())
-            view->updateAll();
-
       scene()->update();
-      score->setUpdateAll();
-      score->update();
       score->endCmd();
 
       QList<PianoItem*> selectedItems = getSelectedItems();
