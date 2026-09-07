62f7159e43ca40986b079c6d3355b49c8010d7c4
J.G. <worldwideweary@aol.com>
2026-09-07 21:05:48 +0000

    PRE: Don't restrict Cut/Tie to editing staff, but apply to any note

--- mscore/pianoroll/pianoview.cpp
+++ mscore/pianoroll/pianoview.cpp
@@ -4717,11 +4717,15 @@ void PianoView::cutChord(const QPointF& pos)
 
       PianoItem* pn = pickNote(pickTick, pickPitch);
 
-      if (pn && useOnsetDiamond(pn->note()))
+      if (!pn || !pn->note())
             return;
 
-      const int voice = pn ? pn->note()->voice() : _editNoteVoice;
-      const int track = _staff->idx() * VOICES + voice;
+      Note* note = pn->note();
+
+      if (useOnsetDiamond(note))
+            return;
+
+      const int track = note->track();
 
       const Fraction insertPosition = roundToNearestBeat(pickTick);
 
@@ -4916,12 +4920,16 @@ bool PianoView::cutChordDragSegment(const QPointF& from,
 
             const Fraction cutTick = roundToNearestBeat(pickTick);
 
-            if (useOnsetDiamond(_staff, cutTick))
+            PianoItem* pn = pickNote(pickTick, pickPitch);
+            if (!pn || !pn->note())
                   continue;
 
-            PianoItem* pn = pickNote(pickTick, pickPitch);
-            const int voice = pn ? pn->note()->voice() : _editNoteVoice;
-            const int track = _staff->idx() * VOICES + voice;
+            Note* note = pn->note();
+
+            if (useOnsetDiamond(note))
+                  continue;
+
+            const int track = note->track();
 
             const QPair<Fraction, int> target(cutTick, track);
 
