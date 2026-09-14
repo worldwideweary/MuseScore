ee246dec43b35d9450c86f0b026564a7de449d36
J.G. <worldwideweary@aol.com>
2026-09-14 01:07:25 +0000

    Advanced Preference: Omit adding lines to linked staves when applied from palettes/commands

--- libmscore/mscore.cpp
+++ libmscore/mscore.cpp
@@ -140,6 +140,7 @@ bool    MScore::hoverColorEnabled;
 int     MScore::pedalEventsMinTicks;
 bool    MScore::fadeFocus;
 bool    MScore::currentSystemAlwaysTop;
+bool    MScore::omitAddingLinkedLines;
 
 bool    MScore::cursorMoveByBeat;
 bool    MScore::cursorMoveByMeasure;
