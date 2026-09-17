7ed18964cf2eb4103b50c1a1c7e03738ad7d2af3
J.G. <worldwideweary@aol.com>
2026-09-17 19:19:31 +0000

    Workspace migration: (version 3) Piano Roll Editor
    …also ensure the View menu is updated as with Piano keyboard et al.

--- mscore/workspace.cpp
+++ mscore/workspace.cpp
@@ -46,7 +46,7 @@ int qt_ntfs_permission_lookup;
 
 namespace Ms {
 
-static constexpr int WORKSPACE_UI_VERSION = 2;
+static constexpr int WORKSPACE_UI_VERSION = 3;
 
 bool WorkspacesManager::isWorkspacesListDirty = true;
 Workspace* WorkspacesManager::m_currentWorkspace = nullptr;
@@ -1094,6 +1094,20 @@ void Workspace::migrate(int uiVersion)
                                      "repeat",
                                      InsertPosition::AFTER);
                   mscore->populatePlaybackControls();
+                  }
+            }
+      if (uiVersion < 3) {
+            ensureMenuAction("menu-view",
+                             "toggle-piano-roll",
+                             "toggle-scorecmp-tool");
+
+            std::list<const char*>* entries = mscore->alternativeEntries();
+            if (entries) {
+                  ensureToolbarEntry(*entries,
+                                     "toggle-piano-roll",
+                                     "toggle-piano",
+                                     InsertPosition::AFTER);
+                  mscore->populateAlternativeOperations();
                   }
             }
       }
